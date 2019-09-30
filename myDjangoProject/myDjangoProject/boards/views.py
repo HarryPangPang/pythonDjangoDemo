a6e70ffbb4541605fbe447a89a3b07fbdc710f97
qiang.zhang <qiang.zhang@garena.cn>
2019-09-30 18:24:21 +0800

    a

--- myDjangoProject/myDjangoProject/boards/views.py
+++ myDjangoProject/myDjangoProject/boards/views.py
@@ -2,12 +2,12 @@
 
 # Create your views here.
 # from django.http import HttpResponse
-from django.shortcuts import render,get_object_or_404
-from .models import Board
-from django.http import Http404
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Board, Topic, Post
 
 def home(request):
-    boards = Board.objects.all()
+    boards = Board.objects.all() 
     return render(request,'home.html',{'boards':boards})
 def board_topics(request,pk):
     # try:
@@ -16,3 +16,22 @@ def board_topics(request,pk):
     #     raise Http404
     board = get_object_or_404(Board,pk=pk)
     return render(request,'topics.html',{'board':board})
+def new_topic(request,pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+        user = User.objects.first()  # TODO: get the currently logged in user
+        topic = Topic.objects.create(
+            subject=subject,
+            board = board,
+            starter=user
+        )
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+
+    return render(request,'new_topic.html',{'board':board})
